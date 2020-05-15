9dd355d06a584ca16459e5f9d115e4abc8d7d9e1
KleinPan <964012840@qq.com>
2020-05-15 18:18:52 +0800

    备份

--- One.Core/ExtensionMethods/Enumerate.cs
+++ One.Core/ExtensionMethods/Enumerate.cs
@@ -53,5 +53,23 @@ namespace One.Core.ExtensionMethods
 
             return -1;
         }
+
+
+        /// <summary>
+        ///     Enumerates for each in this collection.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="action">The action.</param>
+        /// <returns>An enumerator that allows foreach to be used to process for each in this collection.</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> @this, Action<T> action)
+        {
+            T[] array = @this.ToArray();
+            foreach (T t in array)
+            {
+                action(t);
+            }
+            return array;
+        }
     }
 }

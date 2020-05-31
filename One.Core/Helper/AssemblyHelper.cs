19a160e97d5bc1e95175575270f8d8991aa462e2
潘文博 <964012840@qq.com>
2020-05-31 20:11:47 +0800

    测试版本

---
+++ One.Core/Helper/AssemblyHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace One.Core.Helper
+{
+   public class AssemblyHelper
+    {
+
+        public static Attribute GetAttribute(Assembly assembly, Type attributeType)
+        {
+            object[] attributes = assembly.GetCustomAttributes(attributeType, false);
+            if (attributes.Length == 0)
+            {
+                return null;
+            }
+
+            return (Attribute)attributes[0];
+        }
+    }
+}

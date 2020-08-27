f349333250adfb9481a57049047b1c06768d45be
潘文博 <964012840@qq.com>
2020-08-27 10:43:08 +0800

    增加计算器Converter

---
+++ One.Control/Converters/CalculatorConverters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace One.Control.Converters
+{
+    public class CalculatorConverters : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double original = (double)value;
+            bool can = double.TryParse(parameter.ToString(), out double add);
+            if (can)
+            {
+                return original + add;
+            }
+            else
+            {
+                return original;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

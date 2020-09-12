1e3a38493547d797364ea93c05b9c5ff83b4f311
潘文博 <964012840@qq.com>
2020-09-12 16:05:29 +0800

    在原有的基础上可以合并Basedon上的Style

--- One.Control/ExtentionMethods/MultiStyleExtension.cs
+++ One.Control/ExtentionMethods/MultiStyleExtension.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Markup;
-using System.Xaml;
-using System.Linq;
 
 namespace One.Control.ExtentionMethods
 {
-    /// <summary>
-    /// 实现一个标记扩展，该标记扩展支持根据 XAML 制作的多个静态（XAML 加载时）<see cref="System.Windows.Style"/> 资源引用。
-    /// </summary>
+    /// <summary> 实现一个标记扩展，该标记扩展支持根据 XAML 制作的多个静态（XAML 加载时） <see cref="System.Windows.Style"/> 资源引用。 </summary>
     [MarkupExtensionReturnType(typeof(Style))]
     public class MultiStyleExtension : MarkupExtension
     {
@@ -39,41 +34,49 @@ namespace One.Control.ExtentionMethods
             }
         }
 
-        /// <summary>
-        /// 构造方法。
-        /// </summary>
+        /// <summary> 构造方法。 </summary>
         public MultiStyleExtension()
         {
-
         }
 
-        /// <summary>
-        /// 构造方法。
-        /// </summary>
-        /// <param name="resourceKeys">多个<see cref="System.Windows.Style"/>资源字典多个 Key</param>
+        /// <summary> 构造方法。 </summary>
+        /// <param name="resourceKeys"> 多个 <see cref="System.Windows.Style"/> 资源字典多个 Key </param>
         public MultiStyleExtension(string resourceKeys)
         {
             ResourceKeys = resourceKeys;
         }
 
-
-        /// <summary>
-        /// 返回一个应在此扩展应用的属性上设置的对象。对于 <see cref="WpfMultiStyle.MultiStyleExtension"/>，这是在资源字典中查找的多个 <see cref="System.Windows.Style"/> 对象，并合并这些对象，其中要查找的对象由 <see cref="System.Windows.StaticResourceExtension.ResourceKey"/> 标识。
-        /// </summary>
-        /// <param name="serviceProvider">可以为标记扩展提供服务的对象。</param>
-        /// <returns>要在计算标记扩展提供的值的属性上设置的对象值。</returns>
+        /// <summary> 返回一个应在此扩展应用的属性上设置的对象。对于 <see cref="WpfMultiStyle.MultiStyleExtension"/>，这是在资源字典中查找的多个 <see cref="System.Windows.Style"/> 对象，并合并这些对象，其中要查找的对象由 <see cref="System.Windows.StaticResourceExtension.ResourceKey"/> 标识。 </summary>
+        /// <param name="serviceProvider"> 可以为标记扩展提供服务的对象。 </param>
+        /// <returns> 要在计算标记扩展提供的值的属性上设置的对象值。 </returns>
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             Style resultStyle = new Style();
 
             IProvideValueTarget service = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
-            var fe = service.TargetObject as FrameworkElement;
 
-            if (fe != null)
+            if (service.TargetObject is FrameworkElement fe)
+            {
+                if (fe != null)
+                {
+                    foreach (string resourceKey in _internalResourceKeys)
+                    {
+                        Style currentStyle = fe.TryFindResource(resourceKey) as Style;
+
+                        // 忽略无效的 Style
+                        if (currentStyle != null)
+                        {
+                            resultStyle.Merge(currentStyle);
+                        }
+                    }
+                }
+            }
+            //可以合并Basedon上的style
+            else if (service.TargetObject is Style style)
             {
                 foreach (string resourceKey in _internalResourceKeys)
                 {
-                    Style currentStyle = fe.TryFindResource(resourceKey) as Style;
+                    Style currentStyle = Application.Current.TryFindResource(resourceKey) as Style;
 
                     // 忽略无效的 Style
                     if (currentStyle != null)
@@ -85,7 +88,5 @@ namespace One.Control.ExtentionMethods
 
             return resultStyle;
         }
-
-
     }
 }

bef6748ada984e08397cb179e9d9f4fb9e088b5a
潘文博 <964012840@qq.com>
2020-12-24 15:15:11 +0800

    增加串口用户控件，待测试

---
+++ One.Control/EnumConverters/EnumDescriptionTypeConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.Control.EnumConverters
+{
+    public class EnumDescriptionTypeConverter : EnumConverter
+    {
+        public EnumDescriptionTypeConverter(Type type)
+            : base(type)
+        {
+        }
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                if (value != null)
+                {
+                    FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+                    if (fieldInfo != null)
+                    {
+                        var attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                        return ((attributes.Length > 0) && (!String.IsNullOrEmpty(attributes[0].Description))) ? attributes[0].Description : value.ToString();
+                    }
+                }
+
+                return string.Empty;
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}

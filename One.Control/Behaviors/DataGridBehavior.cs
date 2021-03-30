ac1215e77fd96ed60469e821daed5e27576e2218
潘文博 <964012840@qq.com>
2021-03-30 17:46:31 +0800

    增加datagrid自动滚动附加属性

--- One.Control/Behaviors/DataGridBehavior.cs
+++ One.Control/Behaviors/DataGridBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -10,6 +11,10 @@ namespace One.Control.Behaviors
     /// <summary> 使用方法 左尖括号 DataGrid ItemsSource="{Binding ...}" behaviors:DataGridBehavior.DisplayRowNumber="True" 右尖括号 </summary>
     public class DataGridBehavior
     {
+         
+
+       
+
         #region DisplayRowNumber
 
         public static DependencyProperty DisplayRowNumberProperty =
@@ -89,5 +94,98 @@ namespace One.Control.Behaviors
         }
 
         #endregion Get Visuals
+
+        #region 自动滚动
+        public static readonly DependencyProperty AutoscrollProperty = DependencyProperty.RegisterAttached(
+           "Autoscroll", typeof(bool), typeof(DataGridBehavior), new PropertyMetadata(default(bool), AutoscrollChangedCallback));
+
+        private static readonly Dictionary<DataGrid, NotifyCollectionChangedEventHandler> handlersDict = new Dictionary<DataGrid, NotifyCollectionChangedEventHandler>();
+
+        private static void AutoscrollChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            var dataGrid = dependencyObject as DataGrid;
+            if (dataGrid == null)
+            {
+                throw new InvalidOperationException("Dependency object is not DataGrid.");
+            }
+
+            if ((bool)args.NewValue)
+            {
+                Subscribe(dataGrid);
+                dataGrid.Unloaded += DataGridOnUnloaded;
+                dataGrid.Loaded += DataGridOnLoaded;
+            }
+            else
+            {
+                Unsubscribe(dataGrid);
+                dataGrid.Unloaded -= DataGridOnUnloaded;
+                dataGrid.Loaded -= DataGridOnLoaded;
+            }
+        }
+
+        private static void Subscribe(DataGrid dataGrid)
+        {
+            var handler = new NotifyCollectionChangedEventHandler((sender, eventArgs) => ScrollToEnd(dataGrid));
+
+            if (!handlersDict.ContainsKey(dataGrid))
+            {
+                handlersDict.Add(dataGrid, handler);
+                ((INotifyCollectionChanged)dataGrid.Items).CollectionChanged += handler;
+                ScrollToEnd(dataGrid);
+            }
+
+
+        }
+
+        private static void Unsubscribe(DataGrid dataGrid)
+        {
+            NotifyCollectionChangedEventHandler handler;
+            handlersDict.TryGetValue(dataGrid, out handler);
+            if (handler == null)
+            {
+                return;
+            }
+            ((INotifyCollectionChanged)dataGrid.Items).CollectionChanged -= handler;
+            handlersDict.Remove(dataGrid);
+        }
+
+        private static void DataGridOnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var dataGrid = (DataGrid)sender;
+            if (GetAutoscroll(dataGrid))
+            {
+                Subscribe(dataGrid);
+            }
+        }
+
+        private static void DataGridOnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var dataGrid = (DataGrid)sender;
+            if (GetAutoscroll(dataGrid))
+            {
+                Unsubscribe(dataGrid);
+            }
+        }
+
+        private static void ScrollToEnd(DataGrid datagrid)
+        {
+            if (datagrid.Items.Count == 0)
+            {
+                return;
+            }
+            datagrid.ScrollIntoView(datagrid.Items[datagrid.Items.Count - 1]);
+        }
+
+        public static void SetAutoscroll(DependencyObject element, bool value)
+        {
+            element.SetValue(AutoscrollProperty, value);
+        }
+
+        public static bool GetAutoscroll(DependencyObject element)
+        {
+            return (bool)element.GetValue(AutoscrollProperty);
+        }
+
+        #endregion
     }
 }

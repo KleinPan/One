295e948e18049ed1f2d7c19a8c2503342277aa6e
潘文博 <964012840@qq.com>
2021-05-20 09:23:29 +0800

    增加IntToBoolConverter，删除必须要的库

---
+++ One.Control/Converters/IntToBoolConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace One.Control.Converters
+{
+    /// <summary>
+    /// 常用来绑定带参数的checkbox
+    /// </summary>
+    public class IntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+
+            int source = System.Convert.ToInt32(value);
+            int para = System.Convert.ToInt32(parameter);
+
+            if (source == para)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int source = System.Convert.ToInt32(value);
+            int para = System.Convert.ToInt32(parameter);
+
+            if (source == para)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

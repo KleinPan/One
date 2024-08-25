9aadd7aee2967b0f8ca47289d37fdf7f04671b33
KleinPan <964012840@qq.com>
2024-08-25 21:02:20 +0800

    新增检测文件被锁功能备份；

---
+++ One.Toolbox/ExtensionMethods/EMForRstrtMgr.cs
@@ -0,0 +1,20 @@
+﻿using One.Toolbox.Helpers;
+
+namespace One.Toolbox.ExtensionMethods;
+
+
+public static class EMForRstrtMgr
+{
+    /// <summary> 复制指定长度的字符到新的字符串 </summary>
+    /// <param name="str">    </param>
+    /// <param name="start">  </param>
+    /// <param name="length"> </param>
+    /// <returns> </returns>
+    public static void Judge(this Vanara.PInvoke.Win32Error error)
+    {
+        if (error != Vanara.PInvoke.Win32Error.ERROR_SUCCESS)
+        {
+            MessageShowHelper.ShowErrorMessage($"Error Code:{error.ToString()}");
+        }
+    }
+}
---
+++ One.Toolbox/ViewModels/FileMonitor/FileMonitorPageVM.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using One.Toolbox.ExtensionMethods;
+using One.Toolbox.Helpers;
+using One.Toolbox.ViewModels.Base;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.RstrtMgr;
+
+namespace One.Toolbox.ViewModels.FileMonitor;
+
+public partial class FileMonitorPageVM : BaseVM
+{
+    public FileMonitorPageVM() { }
+
+    public override void OnNavigatedEnter()
+    {
+        base.OnNavigatedEnter();
+        InitData();
+    }
+
+    void InitData() { }
+
+    public ObservableCollection<FIleInUseVM> FIleInUseVMs { get; set; } =
+        new ObservableCollection<FIleInUseVM>();
+
+    private void TestFile(string filePath)
+    {
+
+        FIleInUseVMs.Clear();
+        StringBuilder stringBuilder = new StringBuilder();
+        RmStartSession(out uint pSessionHandel, 0, stringBuilder).Judge();
+
+        RmRegisterResources(pSessionHandel, 1, [filePath], 0, null, 0, null).Judge();
+
+        uint nProcInfo = 10;
+        RM_PROCESS_INFO[] rM_PROCESS_INFOs = new RM_PROCESS_INFO[nProcInfo];
+        RmGetList(
+                pSessionHandel,
+                out uint nProcInfoNeeded,
+                ref nProcInfo,
+                rM_PROCESS_INFOs,
+                out RM_REBOOT_REASON lpdwRebootReasons
+            )
+            .Judge();
+
+        if (nProcInfoNeeded == 0)
+        {
+            MessageShowHelper.ShowInfoMessage("This file is not in locked!");
+        }
+
+        foreach (var item in rM_PROCESS_INFOs)
+        {
+            var fIleInUseVM = new FIleInUseVM();
+            fIleInUseVM.LockFileName = item.strAppName;
+            fIleInUseVM.LockProcessID = item.Process.dwProcessId;
+            FIleInUseVMs.Add( fIleInUseVM );
+        }
+    }
+}
+
+public partial class FIleInUseVM : BaseVM
+{
+    [ObservableProperty]
+    private string lockFileName;
+
+    [ObservableProperty]
+    private uint lockProcessID;
+}
